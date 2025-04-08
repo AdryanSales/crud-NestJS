4c3e03cdb3c4eac9a5673dea44c486682d3e3007
AdryanSales <adryan.sales21@gmail.com>
2025-04-08 16:28:42 -0300

    FIX: new custom decorator and simple adjusts

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,50 +1,40 @@
 import { 
     Controller,
     Post, Get, Put, Patch, Delete,   // CRUD
-    Param, Body,
-    ParseIntPipe,
-    UseInterceptors
+    Body
 } from "@nestjs/common";
 import { CreateUserDTO } from "./dto/create-user.dto";
 import { OverwriteUserDTO } from "./dto/overwrite-user.dto";
 import { UpdateUserDTO } from "./dto/update-user.dto";
 import { UserSevice } from "./user.service";
-// import { LogInterceptor } from "src/interceptors/log.interceptor"; // só importe se for utilizar nesse arquivo
+import { ParamId } from "src/decorators/param-id.decorator";
 
-// Podemos usar Interceptors nos controles
-// @UseInterceptors(LogInterceptor)
 @Controller('users')
 export class UserController {
     constructor(private readonly userService: UserSevice){}
 
-    // Podemos usar Interceptors nos métodos
-    // @UseInterceptors(LogInterceptor)
     @Post()
     async create(@Body() data:CreateUserDTO) {
         return this.userService.create(data);
     }
-    @Get() // mostra todos os usuarios cadastrados
+    @Get()
     async read() {
         return this.userService.read();
     }
-    @Get(":id") // mostra apenas um usuário com base no id
-    async readOne(@Param('id', ParseIntPipe) id: number) {
+    @Get(":id")
+    async readOne(@ParamId() id: number) {
         return this.userService.readOne(id);
     }
     @Put(":id") 
-    async overwrite(@Body() data:OverwriteUserDTO, @Param('id', ParseIntPipe) id: number) {
-        //  exige que todas as informações sejam passadas, do contrário, 
-        //  se deixar em branco irá sobrescrever as informações já existentes, deixando vazio, 
-        //  mas podemos montar uma validação pra cada campo, evitando isso
+    async overwrite(@Body() data:OverwriteUserDTO, @ParamId() id: number) {
         return this.userService.overwrite(id, data);
     }
     @Patch(":id")
-    async update(@Body() data:UpdateUserDTO, @Param('id', ParseIntPipe) id) {
+    async update(@Body() data:UpdateUserDTO, @ParamId() id) {
         return this.userService.update(id, data);
     }
     @Delete(":id")
-    //  importante de se utilizar pipes (tubos), para transformar string em number
-    async delete(@Param('id', ParseIntPipe) id: number) {
+    async delete(@ParamId() id: number) {
         return this.userService.delete(id)
     }
 }
