981b2ecae8f582ef66a1446b231535299a4de051
AdryanSales <adryan.sales21@gmail.com>
2025-04-08 10:55:47 -0300

    view method of requests

--- src/interceptors/log.interceptor.ts
+++ src/interceptors/log.interceptor.ts
@@ -9,6 +9,7 @@ export class LogInterceptor implements NestInterceptor {
             const request = context.switchToHttp().getRequest();
             
             console.log(`URL: ${request.url}`) //visualizar rota utilizada para requisição
+            console.log(`METHOD: ${request.method}`) //visualizar método utilizado para requisição
             console.log(`Execução levou: ${Date.now() - dt} milisegundos.`)
         } ))
     }
