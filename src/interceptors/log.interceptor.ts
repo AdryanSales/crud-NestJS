77179755f9d04fe83c64f9c8ba984adfe304ce68
AdryanSales <adryan.sales21@gmail.com>
2025-04-08 10:47:34 -0300

    view URL used to global requests

--- src/interceptors/log.interceptor.ts
+++ src/interceptors/log.interceptor.ts
@@ -6,6 +6,9 @@ export class LogInterceptor implements NestInterceptor {
     intercept(context: ExecutionContext, next:CallHandler): Observable<any> {
         const dt = Date.now();
         return next.handle().pipe(tap( () => {
+            const request = context.switchToHttp().getRequest();
+            
+            console.log(`URL: ${request.url}`) //visualizar rota utilizada para requisição
             console.log(`Execução levou: ${Date.now() - dt} milisegundos.`)
         } ))
     }
