f0c02eea6892c4e8bb2ffc5e60036af3af304fbb
AdryanSales <adryan.sales21@gmail.com>
2025-05-26 12:50:19 -0300

    refactor: ORM  adaptations from prisma to typeorm

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,18 +2,19 @@ import { forwardRef, Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
-import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthService } from './auth.service';
 import { FileModule } from 'src/file/file.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/user/entity/user-entity';
 
 @Module({
   imports: [
     JwtModule.register({
-      secret: process.env.JWT_SECRET, //senha gerada no site // armazenada em um avariavel de ambiente
+      secret: String(process.env.JWT_SECRET), //senha gerada no site // armazenada em uma variavel de ambiente
     }),
     forwardRef(() => UserModule),
-    PrismaModule, // permite o acesso ao banco de dados
     FileModule,
+    TypeOrmModule.forFeature([UserEntity]),
   ],
   controllers: [AuthController],
   providers: [AuthService],
---
+++ src/user/entity/user-entity.ts
@@ -0,0 +1,52 @@
+import { Role } from 'src/enums/role.enum';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({
+  name: 'users',
+})
+export class UserEntity {
+  @PrimaryGeneratedColumn({
+    unsigned: true,
+  })
+  id: number;
+
+  @Column({
+    length: 63,
+  })
+  name: string;
+
+  @Column({
+    length: 127,
+    unique: true,
+  })
+  email: string;
+
+  @Column({
+    type: 'date',
+    nullable: true,
+  })
+  birthAt: Date;
+
+  @Column({
+    default: Role.User,
+  })
+  role: number;
+
+  @Column({
+    length: 255,
+    nullable: false,
+  })
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: string;
+
+  @UpdateDateColumn()
+  updatedAt: string;
+}
